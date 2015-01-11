78512a857ed7c882708b60c6f425dc8bf9846dc3
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-01-11 14:47:48 +0000

    Add Demo Page for Framework

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Need a professional Webhost? Let us do I.T.">
+	<title>
+		Guru Web Framework - Demo Page
+	</title>
+	<link href="../css/framework.css" rel="stylesheet" media="screen">
+	<link media="all" type="text/css" rel="stylesheet" href="../css/main.css">
+	<!--[if lt IE 9]>
+		<script src="bootstrap/assets/js/html5shiv.js"></script>
+		<script src="bootstrap/assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header>
+		<img src="img/logo.png" alt="logo">
+		<h1>Header</h1>
+		<nav>
+			<ul>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+			</ul>
+		</nav>
+	</header>
+	<h1>Heading 1</h1>
+	<h2>Heading 2</h2>
+	<h3>Heading 3</h3>
+	<img src="img/logo.png" alt="logo">
+	<footer>
+		<h1>I'm a footer</h1>
+	</footer>
+</body>
