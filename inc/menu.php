c1b3414995225cb6008e3b4e9d0e9a0a83c91f5c
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-11 17:44:25 +0000

    menu pipes added

--- inc/menu.php
+++ inc/menu.php
@@ -2,7 +2,11 @@
 	$menu = array("About", "Server Status", "Free Hosting", "Premium Hosting", "Design Services", "Contact");
 	echo '<ul>'."\n";
 	foreach ($menu as $item) {
-		echo '<li>'.$item.'</li>'."\n";
+		if ($item != "Contact"){
+			echo '<li>'.$item.'</li>'.' | '."\n";
+		} else {
+			echo '<li>'.$item.'</li>'."\n";
+		}
 	}
 	echo "</ul>"."\n";
 ?>
