b1b9dc5412eaf6ad3b2c8eeea7ae1890814f773e
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-11 17:18:50 +0000

    menu float right

--- inc/menu.php
+++ inc/menu.php
@@ -2,7 +2,7 @@
 	$menu = array("About", "Server Status", "Free Hosting", "Premium Hosting", "Design Services", "Contact");
 	echo "<ul>"."\n";
 	foreach ($menu as $item) {
-		echo "<li>".$item."</li>";
+		echo '<li class="menu_fr">'.$item.'</li>';
 	}
 	echo "</ul>";
 ?>
