c323b53e35f245fd4c076fce14ecf1253c1f37c7
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-01-11 16:24:05 +0000

    pho ul creator

--- inc/menu.php
+++ inc/menu.php
@@ -2,7 +2,7 @@
 	$menu = array("About", "Server Status", "Free Hosting", "Premium Hosting", "Design Services", "Contact");
 	echo "<ul>"."\n";
 	foreach ($menu as $item) {
-		echo "<li>".$item."</li>"
+		echo "<li>".$item."</li>";
 	}
-	echo "</ul>"
+	echo "</ul>";
 ?>
